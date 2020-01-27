b558ee019003070d4e8c257c0e7165af8c47c63d
schlevik <vtschlegel@gmail.com>
2020-01-27 16:35:22 +0000

    add albert on squad

--- stresstest/model.py
+++ stresstest/model.py
@@ -3,6 +3,7 @@ from typing import Tuple
 from allennlp.predictors import Predictor
 from allennlp.pretrained import bidirectional_attention_flow_seo_2017
 from allennlp.pretrained import naqanet_dua_2019
+from transformers import AlbertForQuestionAnswering, AlbertTokenizer
 
 
 def bidaf() -> Tuple[str, Predictor]:
@@ -11,3 +12,29 @@ def bidaf() -> Tuple[str, Predictor]:
 
 def naqanet() -> Tuple[str, Predictor]:
     return "NaQAnet", naqanet_dua_2019()
+
+
+class Albert:
+    def __init__(self, path: str, gpu=False):
+        self.tokenizer = AlbertTokenizer(path)
+        self.model = AlbertForQuestionAnswering(path)
+        self.gpu = gpu
+
+    def predict(self, question, passage):
+        d = self.tokenizer.encode_plus(question.lower(), passage.lower(),
+                                       return_tensors='pt')
+        if self.gpu:
+            token_type_ids = d['token_type_ids'].cuda()
+            input_ids = d['input_ids'].cuda()
+        else:
+            input_ids = d['input_ids']
+            token_type_ids = d['token_type_ids']
+
+        s, e = self.model(token_type_ids=token_type_ids,
+                          input_ids=input_ids)
+        return " ".join(
+            [
+                self.tokenizer.decode(int(i))
+                for i in d['input_ids'][0][s.argmax():e.argmax() + 1]
+            ]
+        )
