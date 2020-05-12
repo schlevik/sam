5bdc5e579045c52b90c12818525dc6a38b122c5f
schlevik <viktor@schlegel-online.de>
2020-05-12 16:56:49 +0100

    refactor test env

--- tests/testutil.py
+++ tests/testutil.py
@@ -1,7 +1,9 @@
 from copy import deepcopy
+from typing import Tuple, List
 
-from stresstest.classes import Config
+from stresstest.classes import Config, Question
 from stresstest.generate import StoryGenerator
+from stresstest.generate_special.generate_with_modifier import ModifierGenerator
 from stresstest.realize import Realizer
 from tests.resources.templates import sentences, dollar, at, percent, bang, templates
 
@@ -12,46 +14,52 @@ def only(sents, n, action='test'):
     return sents
 
 
-def interactive_env(path='stresstest/resources/config.json', g_class=StoryGenerator, realizer=None, do_print=True):
-    c = Config(path)
-    c.pprint()
-    g = g_class(c)
-    t = realizer or Realizer()
-    ss = g.generate_story()
-    story, visits = t.realise_story(ss, g.world)
-    (single_span_questions, multi_span_questions, unanswerable_questions, abstractive_questions) = g.generate_questions(
-        ss, visits)
-    realised_ssqs = []
-    realised_msqs = []
-    realised_uaqs = []
-    realised_aqs = []
+def get_questions(generator, realizer, events, visits, story) \
+        -> Tuple[List[Question], List[Question], List[Question], List[Question]]:
+    (single_span_questions, multi_span_questions, unanswerable_questions,
+     abstractive_questions) = generator.generate_questions(events, visits)
+
+    for q in single_span_questions + multi_span_questions + unanswerable_questions + abstractive_questions:
+        realizer.realise_question(q, story)
+
+    return single_span_questions, multi_span_questions, unanswerable_questions, abstractive_questions
+
+
+def print_out(story, *questions):
+    print("===STORY===:")
+    print('\n'.join(story))
+    print()
+    print("===QUESTIONS===:")
+    print(len(questions))
+    for q in (q for qs in questions for q in qs):
+        if q.realized:
+            print(q.realized, q.answer if q.answer else "Unanswerable")
+
+
+def env_for_modifier(path='stresstest/resources/config.json', g_class=ModifierGenerator, do_print=True,
+                     do_realise=True):
+    from stresstest.resources.templates_modifier import sentences, dollar, at, percent, bang, templates
+    realizer = Realizer(sentences=sentences, dollar=dollar, at=at, percent=percent, bang=bang,
+                        question_templates=templates, unique_sentences=False)
+
+    return interactive_env(path, g_class, realizer, do_print, do_realise)
+
+
+def interactive_env(path='stresstest/resources/config.json', g_class=StoryGenerator, realizer=None, do_print=True,
+                    do_realise=True):
+    cfg = Config(path)
+    cfg.pprint()
+    generator = g_class(cfg)
+    events = generator.generate_story()
+    if not do_realise:
+        return generator, cfg, events, None, None, None
+    realizer = realizer or Realizer()
+    story, visits = realizer.realise_story(events, generator.world)
+    ssq, maq, uaq, abq = get_questions(generator, realizer, events, visits, story)
+    all_questions = (ssq, maq, uaq, abq)
     if do_print:
-        print("===STORY===:")
-    for q in single_span_questions:
-        rq = t.realise_question(q, story)
-        if rq:
-            realised_ssqs.append(rq)
-    for q in multi_span_questions:
-        rq = t.realise_question(q, story)
-        if rq:
-            realised_msqs.append(rq)
-    for q in unanswerable_questions:
-        rq = t.realise_question(q, story)
-        if rq:
-            realised_uaqs.append(rq)
-    qs = [
-        single_span_questions,
-        multi_span_questions,
-        unanswerable_questions,
-        abstractive_questions
-    ]
-    if do_print:
-        print('\n'.join(story))
-        print()
-        print("===QUESTIONS===:")
-        for q, a in realised_ssqs + realised_msqs + realised_uaqs + realised_aqs:
-            print(q, a if a else "Unanswerable")
-    return g, c, t, story, qs, ss
+        print_out(story, ssq, maq, uaq, abq)
+    return generator, cfg, events, realizer, story, all_questions
 
 
 class TestRealizer(Realizer):
