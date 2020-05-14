dab65c6d061018bffbe480cede08275d4ec5fec4
schlevik <viktor@schlegel-online.de>
2020-05-14 19:18:22 +0100

    create first full pipeline for albert on modifier

--- scripts/compare.py
+++ scripts/compare.py
@@ -16,19 +16,22 @@ from stresstest.util import load_json
 
 @click.command()
 @click.option("--input", type=str, default="data/stresstest.json")
+@click.option("--reference", type=str, default="data/drop_nfl.json")
 @click.option("--output", type=str, default="metrics/compare.json")
-@click.option("--flat", default=False, is_flag=True)
+# @click.option("--flat", default=False, is_flag=True)
 @click.option("--attr", type=str, default='passage')
 @click.option("--metric", type=str, default='Jaccard')
-def compare(input, output, flat, attr, metric):
+def diversity(input, reference, output, attr, metric):
     # TODO: make metrics appendable
     metric_class = quickconf.load_class(metric, restrict_to=Distance, relative_import='stresstest.textmetrics')
     samples = load_json(input)
+    reference = load_json(reference)
     distances = []
     getter = itemgetter(attr)
-    if flat:
-        samples = [samples]
+    # if flat:
+    #    samples = [samples]
     logger.debug(f"Evaluating {len(samples)} sample(s).")
+    # samples
     for i, sample in enumerate(samples):
         logger.debug(f"Sample #{i} has {len(sample)} paragraphs.")
         corpus: Iterable[str] = (getter(s) for s in sample)
@@ -36,19 +39,43 @@ def compare(input, output, flat, attr, metric):
         result = np.array(result)
         # printable_result = f'{result.mean()} ± {2 * result.std() / math.sqrt(len(result))}'
         distances.append(result.mean())
+
     distances = np.array(distances)
     t_975 = t.ppf(1 - .025, df=len(samples) - 1)
-    printable_result = f'{distances.mean():.4f} +/- {t_975 * distances.std() / math.sqrt(len(distances)):.4f}'
+    ci95 = t_975 * distances.std() / math.sqrt(len(distances))
+    printable_result = f'{distances.mean():.4f} +/- {ci95:.4f}'
     click.echo(f"Pointwise average distance under the {click.style(metric, fg='green')} metric: "
                f"{click.style(printable_result, fg='green', bold=True)}")
+
+    # reference
+    corpus_reference: Iterable[str] = (getter(s) for s in reference)
+    result_reference = np.array(pointwise_average_distance(corpus_reference, metric_class()))
+    printable_result_reference = f'{result_reference.mean():.4f}'
+
+    click.echo(f"Reference pointwise average distance under the {click.style(metric, fg='green')} metric: "
+               f"{click.style(printable_result_reference, fg='green', bold=True)}")
+    result_reference = result_reference.mean()
+
+    within_ci = abs(distances.mean() - result_reference) < t_975 * distances.std() / math.sqrt(len(distances))
     result = {
         "num_samples": len(samples),
         metric: {
-            'human_readable': printable_result,
-            'mean': distances.mean(),
-            'variance': distances.var(),
-            '95ci': t_975 * distances.std() / math.sqrt(len(distances))
-        }
+            'ours': {
+                'human_readable': printable_result,
+                'mean': distances.mean(),
+                'variance': distances.var(),
+                '95ci': ci95,
+            },
+            "reference": {
+                'human_readable': printable_result_reference,
+                'mean': result_reference,
+            },
+            "difference": {
+                "difference": distances.mean() - result_reference,
+                "within_ci": bool(within_ci)
+            }
+        },
+
     }
     write_json(result, output)
 
---
+++ scripts/final.py
@@ -0,0 +1,61 @@
+import json
+from collections import defaultdict
+
+import click
+import quickconf
+from loguru import logger
+from tqdm import tqdm
+
+from stresstest.classes import Model
+from stresstest.util import load_json, sample_iter, num_questions
+
+
+@click.command()
+@click.option("--diversity", type=str, default='metrics/diversity.json')
+@click.option("--count", type=str, default='metrics/count.json')
+@click.option("--baseline", type=str, default='metrics/score-baseline.json')
+@click.option("--intervention", type=str, default='metrics/score-intervention.json')
+@click.option("--naturality", type=str, default='metrics/naturality.json')
+def report(diversity, count, baseline, intervention, naturality):
+    diversity_diff = load_json(diversity)
+    count = load_json(count)
+    phenomenon_score = load_json(intervention)
+    baseline_score = load_json(baseline)
+    try:
+        naturality_diff = load_json(naturality)
+    except:
+        naturality_diff = None
+    # TODO
+    click.secho("Diversity: ", fg='green')
+    for metric, v in diversity_diff.items():
+        if metric != 'num_samples':
+            click.echo(f'For "{metric}" metric:')
+            click.echo(f"Ours: {str(v['ours']['human_readable'])} Reference: {str(v['reference']['human_readable'])}")
+            click.echo(f"Difference: {v['difference']['difference']}")
+            click.echo(f"Within Confidence Interval? "
+                       f"{click.style('yes', fg='green') if v['difference']['within_ci'] else click.style('no', fg='red')}")
+
+    click.secho("Configuration estimate: ", fg='green')
+    for k, v in count.items():
+        click.echo(f'For "{k}" event type:')
+        click.echo(f"Upper bound: {v['upper']} Lower bound: {v['lower']}")
+
+    if naturality_diff:
+        click.secho("Naturality: ", fg='green')
+        click.echo(f"{naturality_diff}")
+
+    click.secho("Baseline performance: ", fg='green')
+    for metric, v in baseline_score['full'].items():
+        click.secho(f"{metric}: {v['human_readable']}")
+
+    click.secho("Performance after Intervention: ", fg='green')
+    for metric, v in phenomenon_score['full'].items():
+        click.secho(v['human_readable'])
+    click.secho("Differences", fg='green')
+    for (metric, baseline_v) in baseline_score['full'].items():
+        click.secho(f"{metric}: {baseline_v['mean'] - phenomenon_score['full'][metric]['mean']:.4f} ")
+    # click.secho("Difference after intervention: ", fg='green')
+    # click.echo(f"{phenomenon_diff['difference']:.2f}")
+    # TODO well technically we should do a different computation here
+    # click.echo(f"Statistically significant? f"{click.style('yes', fg='green')
+    # if phenomenon_diff['within_ci'] else click.style('no', fg='red')}")
