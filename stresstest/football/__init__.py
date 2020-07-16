19066a42fd85d1476a886f8927f764b93a701a87
schlevik <viktor@schlegel-online.de>
2020-07-16 15:13:45 +0100

    amend football bundle for interactive env

--- stresstest/football/__init__.py
+++ stresstest/football/__init__.py
@@ -1,20 +1,28 @@
 from stresstest.classes import Bundle
 from stresstest.football.football_generator import FootballGenerator
 from stresstest.football.generate_with_modifier import FootballModifierGenerator
-from stresstest.football.resources import templates
-from stresstest.football.resources import templates_modifier
+
+from stresstest.util import do_import
 
 
 def _get_templates_dict(module):
     return {
-        name: getattr(module, name) for name in
+        name: do_import(name, f"{module.__name__}.{name}") for name in
         ['dollar', 'sentences', 'at', 'percent', 'bang', 'question_templates']
     }
 
 
-bundle = Bundle(
-    generator=FootballGenerator,
-    templates=_get_templates_dict(templates),
-    generator_modifier=FootballModifierGenerator,
-    templates_modifier=_get_templates_dict(templates_modifier)
-)
+def _reload_bundle():
+    # some dynamic reimport stuff
+    from stresstest.football.resources import modifier as modifier, baseline
+    bundle = Bundle(
+        generator=FootballGenerator,
+        templates=_get_templates_dict(baseline),
+        generator_modifier=FootballModifierGenerator,
+        templates_modifier=_get_templates_dict(modifier)
+    )
+    del modifier, baseline
+    return bundle
+
+
+bundle = _reload_bundle()
