ea2bcf69ff6afccdafe264e3ff96582c0fc55684
schlevik <viktor@schlegel-online.de>
2020-08-03 15:44:09 +0100

    add t5 train and eval skeleton

--- scripts/dvc_skeleton.py
+++ scripts/dvc_skeleton.py
@@ -5,26 +5,35 @@ import click
 from scripts.utils import get_output_predictions_file_name
 
 
+def get_predictions_folder(sam_path, dataset_name):
+    return f"data/football/{sam_path}/predictions/{dataset_name}"
+
+
 @click.command()
 @click.argument('command', type=str)
 @click.option('--sam', default='RB')
 @click.option('--dataset-name', default='squad1')
-@click.option('--model-name', default='bert-base-uncased')
-@click.option('--model-type', default='bert')
+#@click.option('--model-name', default='bert-base-uncased')
+#@click.option('--model-type', default='bert')
 @click.option("--train-file", default="train-v1.1.json")
 @click.option("--eval-file", default="dev-v1.1.json")
 @click.option("--batch-size", default=None)
 @click.option("--extra-args", default="--fp16 --save-steps 0 --debug-features")
 @click.option("--notify", type=str, default='viktor.schlegel@manchester.ac.uk')
-def generate_dvc(command, sam, dataset_name, model_name, model_type, train_file, eval_file, batch_size, extra_args,
-                 notify):
+@click.option("model_names", '--model-name', type=str, multiple=True)
+@click.option("model_types", '--model-type', type=str, multiple=True)
+def generate_dvc(command, sam, dataset_name,
+                 #model_name, model_type,
+                 train_file, eval_file, batch_size, extra_args,
+                 notify, model_names, model_types):
+    model_name = model_names[0]
+    model_type = model_types[0]
     sam_path = sam.lower()
     sam_dataset_path = f"data/football/{sam_path}"
-    filter_file = f'{sam_dataset_path}/baseline-filter-{sam_path}.json'
-    baseline_file = f"{sam_dataset_path}/baseline-{dataset_name}-{sam_path}.json"
-    intervention_file = f"{sam_dataset_path}/intervention-{dataset_name}-{sam_path}.json"
-    control_file = f"{sam_dataset_path}/control-{dataset_name}-{sam_path}.json"
-    predictions_folder = f"data/football/{sam_path}/predictions"
+    baseline_file = f"{sam_dataset_path}/baseline-{sam_path}.json"
+    intervention_file = f"{sam_dataset_path}/intervention-{sam_path}.json"
+    control_file = f"{sam_dataset_path}/control-{sam_path}.json"
+    predictions_folder = get_predictions_folder(sam_path, dataset_name)
     model_folder = f"{model_name}-{dataset_name}"
     model_path = f"models/{model_folder}"
     train_path = f"data/datasets/{dataset_name}/{train_file}"
@@ -36,7 +45,6 @@ def generate_dvc(command, sam, dataset_name, model_name, model_type, train_file,
         batch_size = 8 if "large" in model_name else 24
 
     if command == 'train-transformers':
-
         cmd = (
             f"python main.py --debug --notify {notify} train {train_path} --model-path {model_name} "
             f"--model-type {model_type} --eval-file {eval_path} --save-model-folder {model_path} "
@@ -49,16 +57,38 @@ def generate_dvc(command, sam, dataset_name, model_name, model_type, train_file,
             f"{cmd}"
         )
     elif command == 'predict-transformers':
+        models_str = " ".join(f"--model-path models/{mp}-{dataset_name} --model-type {mt}" for mp, mt in
+                              zip(model_names, model_types))
         cmd = (f"python main.py --debug --notify {notify} "
                f"predictions {baseline_file} {intervention_file} {control_file} "
-               f"--model-path {model_path} --model-type {model_type} "
-               f"--out-folder {predictions_folder} '--max-answer-length 10 {extra_args}")
+               f" {models_str} "
+               f"--out-folder {predictions_folder} --max-answer-length 10 {extra_args}")
 
-        stage_name = f"predict-{model_folder}-on-{sam_path}"
+        stage_name = f"predict-transformers-{dataset_name}-on-{sam_path}"
+        deps_str = " ".join(f"-d models/{mp}-{dataset_name}" for mp in model_names)
+        outs_str = ' '.join(
+            f"-o {get_output_predictions_file_name(baseline_file, predictions_folder, f'{mp}-{dataset_name}')} "
+            f"-o {get_output_predictions_file_name(intervention_file, predictions_folder, f'{mp}-{dataset_name}')} "
+            f"-o {get_output_predictions_file_name(control_file, predictions_folder, f'{mp}-{dataset_name}')}"
+            for mp in model_names
+        )
+        dvc_cmd = (f"dvc run -n {stage_name} {deps_str} -d {model_path} -d {baseline_file} "
+                   f"-d {intervention_file} -d {control_file}  {outs_str} {cmd}")
+    # elif command == 'predict-transformers-filter':
+    #     predictions_folder = get_predictions_folder(sam_path, 'filter')
+    #     assert len(model_names) == len(model_types)
+    #     models_str = " ".join(f"--model-path models/{mp}-{dataset_name} --model-type {mt}" for mp, mt in
+    #                           zip(model_names, model_types))
+    #     cmd = (f"python main.py --debug --notify {notify} "
+    #            f"predictions {filter_file} "
+    #            f"{models_str} "
+    #            f"--out-folder {predictions_folder} --max-answer-length 10 {extra_args}")
 
-        dvc_cmd = (f"dvc run -n {stage_name} -d scripts/predict_transformers.py -d {model_path} -d {baseline_file} "
-                   f"-d {intervention_file} -d {control_file} -o {baseline_predictions} -o {control_predictions} "
-                   f"-o {intervention_predictions}  {cmd}")
+        # stage_name = f"filter-transformers-{dataset_name}"
+        # deps_str = " ".join(f"-d models/{mp}-{dataset_name}" for mp in model_names)
+        # outs_str = ' '.join(
+        #     f"-o {get_output_predictions_file_name(filter_file, predictions_folder, mp)}" for mp in model_names)
+        # dvc_cmd = f"dvc run -n {stage_name} {deps_str} -d {filter_file} {outs_str} {cmd}"
     elif command == 'evaluate':
         eoi_metric_file = f"{dataset_name}-{sam_path}.json"
         eoi_metric_path = f"metrics/{eoi_metric_file}"
@@ -70,20 +100,20 @@ def generate_dvc(command, sam, dataset_name, model_name, model_type, train_file,
         dvc_cmd = (f"dvc run -n {stage_name} -d {baseline_file} -d {intervention_file} -d {control_file} "
                    f"-d scripts/evaluate_intervention.py -d {predictions_folder} -M {eoi_metric_path} {cmd}")
     elif command == "generate":
-        conf_name = f'conf/{dataset_name}.json'
-        cmd = (f"python main.py generate-balanced --config {conf_name} --seed 56 "
+        #conf_name = f'conf/{dataset_name}.json'
+        cmd = (f"python main.py generate-balanced --config conf/evaluate.json --seed 56 "
                f"--num-workers 8 --do-save --out-path {sam_dataset_path} --modifier-type {sam}")
         stage_name = f"generate-{sam_path}-for-{dataset_name}"
         dvc_cmd = (f"dvc run -n {stage_name} -d scripts/generate_balanced.py "
-                   f"-d {conf_name} -o {baseline_file} -o {intervention_file} -o {control_file} {cmd}")
-    elif command == 'filter':
-        # todo: evaluate all trained on dataset X on confs/test.json
-        stage_name = f'filter-{dataset_name}'
-        metric_file = f'metrics/{dataset_name}-filter.json'
-        cmd = (f"python main.py evaluate {filter_file} --prediction-folder {predictions_folder} "
-               f"--output -M {metric_file} --metric EM,F1 --split-reasoning")
-        dvc_cmd = (f"dvc run -n {stage_name} -d {filter_file} -d {predictions_folder}"
-                   f"-M {metric_file} {cmd}")
+                   f"-d conf/evaluate.json -o {baseline_file} -o {intervention_file} -o {control_file} {cmd}")
+    # elif command == 'filter':
+    #     predictions_folder = get_predictions_folder(sam_path, 'filter')
+    #     stage_name = f'filter-{dataset_name}'
+    #     metric_file = f'metrics/{dataset_name}-filter.json'
+    #     cmd = (f"python main.py evaluate {filter_file} --prediction-folder {predictions_folder} "
+    #            f"--output -M {metric_file} --metric EM,F1 --split-reasoning")
+    #     dvc_cmd = (f"dvc run -n {stage_name} -d {filter_file} -d {predictions_folder}"
+    #                f"-M {metric_file} {cmd}")
     elif command == 'predict-allennlp':
         stage_name = f"predict-{model_name}-on-{dataset_name}"
         model_path = os.path.join(model_path, "model.tar.gz")
@@ -112,7 +142,9 @@ def generate_dvc(command, sam, dataset_name, model_name, model_type, train_file,
         )
         dvc_cmd = (
             f"dvc run -n {stage_name} -d {baseline_file} "
-            f"-d {intervention_file} -d {control_file} -o {predictions_folder}"
+            f"-d {intervention_file} -d {control_file} -o {get_output_predictions_file_name(baseline_file, predictions_folder, 'random')}"
+            f"-o {get_output_predictions_file_name(baseline_file, predictions_folder, 'random')}"
+            f"-o {get_output_predictions_file_name(baseline_file, predictions_folder, 'random')}"
         )
     elif command == 'train-allennlp':
         cmd = f"TRAIN_SET={train_path} EVAL_SET={eval_path} CUDA=0 " \
@@ -127,7 +159,7 @@ def generate_dvc(command, sam, dataset_name, model_name, model_type, train_file,
         raise NotImplementedError()
     elif command == 'train-t5':
 
-        cmd = (f"PYTHONPATH='.' python scripts/t5.py --model_name_or_path {model_name} --output_dir {model_folder} "
+        cmd = (f"PYTHONPATH='.' python scripts/t5.py --model_name_or_path {model_name} --output_dir {model_path} "
                f"--train_file {train_path} --eval_file {eval_path} "
                f"--do_train --do_eval --num_workers 8 --per_device_train_batch_size {batch_size} "
                f"{extra_args}")
