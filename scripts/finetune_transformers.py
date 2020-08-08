74b0027c7c30d9757c5c125dc2cac20e17a0175f
schlevik <viktor@schlegel-online.de>
2020-08-08 09:42:53 +0100

    add evaluate on original dev set to finetune

--- scripts/finetune_transformers.py
+++ scripts/finetune_transformers.py
@@ -1,6 +1,4 @@
 import json
-import logging
-import tempfile
 from collections import defaultdict
 from copy import deepcopy
 
@@ -9,11 +7,10 @@ from loguru import logger
 import torch
 from ax.service.ax_client import AxClient
 from tqdm import trange
-from transformers.data.metrics.squad_metrics import squad_evaluate
 
 from scripts.predict_transformers import evaluate
 from scripts.train_transformers import do_train
-from scripts.utils_transformers import load_examples, Args, get_model, get_tokenizer, set_seed, \
+from scripts.utils_transformers import Args, get_model, get_tokenizer, set_seed, \
     debug_features_examples_dataset, load_or_convert
 from scripts.utils import write_json, get_baseline_intervention_control_from_baseline
 from stresstest.eval_utils import align, evaluate_intervention, get_mean_var_ci_bernoulli, get_mean_var_ci, EMRelaxed
@@ -21,9 +18,9 @@ from stresstest.util import load_json
 
 
 def train_and_eval_single_step(args: Args, train_dataset, aligned_baseline, aligned_intervention, aligned_control,
-                               baseline_dataset, intervention_dataset, control_dataset, baseline_gold_path,
-                               intervention_gold_path, control_gold_path, run_nr=0, keep_predictions=False,
-                               train=True, num_runs=1, evaluate_on='eoi'):
+                               baseline_dataset, intervention_dataset, control_dataset, original_dev_dataset,
+                               baseline_gold_path, intervention_gold_path, control_gold_path,
+                               run_nr=0, train=True, num_runs=1, evaluate_on='eoi'):
     results = []
     for i in range(num_runs):
         set_seed(args)
@@ -36,22 +33,21 @@ def train_and_eval_single_step(args: Args, train_dataset, aligned_baseline, alig
         if train:
             step, loss = do_train(args, train_dataset, model, tokenizer)
 
-        if keep_predictions:
-            args.eval_file = baseline_gold_path
+        args.eval_file = baseline_gold_path
         if evaluate_on == 'eoi' or evaluate_on == 'baseline':
             baseline_predictions = evaluate(args, model, tokenizer, *baseline_dataset, f'baseline-{run_nr}',
                                             return_raw=True)
-        if keep_predictions:
-            args.eval_file = intervention_gold_path
+        args.eval_file = intervention_gold_path
 
         if evaluate_on == 'eoi' or evaluate_on == 'intervention':
             intervention_predictions = evaluate(args, model, tokenizer, *intervention_dataset, f'intervention-{run_nr}',
                                                 return_raw=True)
-        if keep_predictions:
-            args.eval_file = control_gold_path
+
+        args.eval_file = control_gold_path
         if evaluate_on == 'eoi':
             control_predictions = evaluate(args, model, tokenizer, *control_dataset, f'control-{run_nr}',
                                            return_raw=True)
+
             # obtain predictions on all three of them
             (overall_results, results_baseline, results_intervention, results_control,
              correct_before_intervention, correct_change_correct, correct_keep_wrong, correct_change_wrong,
@@ -62,22 +58,31 @@ def train_and_eval_single_step(args: Args, train_dataset, aligned_baseline, alig
             mean, *_ = get_mean_var_ci_bernoulli(overall_results)
             # there's no point to evaluate multiple times if not training
 
-            results.append({
+            result = {
                 "overall": mean,
+                'consistency': len(correct_change_correct) / len(aligned_baseline),
+                'consistency+control': len(correct_baseline_control_intervention) / len(aligned_baseline),
+
                 "acc_baseline": sum(results_baseline) / len(results_baseline),
                 "acc_intervention": sum(results_intervention) / len(results_intervention),
                 "acc_control": sum(results_control) / len(results_control),
                 'correct->change->correct': len(correct_change_correct),
                 'correct(baseline+control)/correct(baseline)': len(correct_baseline_control) / sum(results_baseline),
                 'correct+control->change->correct': len(correct_baseline_control_intervention),
-            })
+            }
         elif evaluate_on == 'baseline':
             metric_results = EMRelaxed(max_length=args.max_answer_length)(aligned_baseline, baseline_predictions)
-            results.append({"EMRelaxed": get_mean_var_ci_bernoulli(metric_results)[0]})
+            result = {"EMRelaxed": get_mean_var_ci_bernoulli(metric_results)[0]}
         elif evaluate_on == 'intervention':
             metric_results = EMRelaxed(max_length=args.max_answer_length)(aligned_baseline, baseline_predictions)
-            results.append({"EMRelaxed": get_mean_var_ci_bernoulli(metric_results)[0]})
-
+            result = {"EMRelaxed": get_mean_var_ci_bernoulli(metric_results)[0]}
+        else:
+            raise NotImplementedError()
+        if original_dev_dataset is not None:
+            original_dev_result = evaluate(args, model, tokenizer, *original_dev_dataset, f'original-dev-{run_nr}',
+                                           return_raw=False)
+            result['original'] = original_dev_result['exact']
+        results.append(result)
     if num_runs == 1:
         return results[0]
 
@@ -133,7 +138,7 @@ def train_and_eval_single_step(args: Args, train_dataset, aligned_baseline, alig
 @click.option("--baseline-gold-file", type=str)
 @click.option("--hyperparams", type=str, default="[]")
 @click.option("--keep-predictions", type=str, default="")
-@click.option('--stfu', type=bool, is_flag=True, default=False)
+@click.option('--mute', type=bool, is_flag=True, default=False)
 @click.option('--max-seq-length', type=int, default=384)
 @click.option('--doc-stride', type=int, default=128)
 @click.option('--max-query-length', type=int, default=64)
@@ -142,16 +147,11 @@ def train_and_eval_single_step(args: Args, train_dataset, aligned_baseline, alig
 @click.option('--do-not-lower-case', type=bool, is_flag=True, default=False)
 @click.option('--runs-per-trial', type=int, default=1)
 @click.option('--evaluate-on', type=str, default='eoi')
-def finetune(**kwargs):
-    evaluate_on = kwargs.pop('evaluate_on')
-    print("evaluate on", evaluate_on)
-    runs_per_trial = kwargs.pop('runs_per_trial')
-    num_hpopt_runs = kwargs.pop('hyperparam_opt_runs')
-    # feature_files = get_baseline_intervention_control_from_baseline(kwargs.pop("baseline_features_file"))
-    out_file = kwargs.pop('out_file')
-    # load eval datasets for predictions
-
-    gold_files = get_baseline_intervention_control_from_baseline(kwargs.pop("baseline_gold_file"))
+@click.option('--original-dev-dataset', type=str, default=None)
+@click.option('--optimize-consistency', is_flag=True, default=False)
+def finetune(optimize_consistency, evaluate_on, original_dev_dataset, runs_per_trial, hyperparam_opt_runs, out_file,
+             mute, baseline_gold_file, hyperparams, keep_predictions, **kwargs):
+    gold_files = get_baseline_intervention_control_from_baseline(baseline_gold_file)
 
     golds = tuple(
         load_json(g) for g in gold_files
@@ -159,19 +159,16 @@ def finetune(**kwargs):
     # load eval gold for evaluation
     aligneds = align(*golds, assert_same=True)
 
-    # run single step
     hyper_params = [{
         'name': hp['name'],
         'type': hp.get("type", 'range'),
         'bounds': hp['bounds'],
         'value_type': hp.get('value_type', 'float'),
         'log_scale': hp.get('log_scale', True)
-    } for hp in json.loads(kwargs.pop('hyperparams'))]
+    } for hp in json.loads(hyperparams)]
 
     logger.info(hyper_params)
-    keep_predictions = kwargs.pop('keep_predictions')
 
-    mute = kwargs.pop('stfu')
     args = Args(**kwargs)
 
     args.debug_features = not mute
@@ -208,6 +205,9 @@ def finetune(**kwargs):
     train_dataset, train_examples, train_features = load_or_convert(args, tokenizer)
     if not mute:
         debug_features_examples_dataset(train_dataset, train_examples, train_features, tokenizer)
+    if original_dev_dataset:
+        args.eval_file = original_dev_dataset
+        original_dev_dataset = load_or_convert(args, tokenizer, evaluate=True)
     ax_client = AxClient()
     ax_client.create_experiment(
         name=f'{args.model_path}@{args.train_file}',
@@ -215,38 +215,44 @@ def finetune(**kwargs):
         objective_name=evaluate_on,
         minimize=False,
     )
-    result = {
-        "trials": [],
-        "tried_params": defaultdict(list),
-        "best_params": ...,
-    }
+    result = {"trials": [], "tried_params": defaultdict(list), "best_params": ...,
+              'pre_eval': train_and_eval_single_step(args, train_dataset, *aligneds, *features, original_dev_dataset,
+                                                     *gold_files, run_nr='eval', train=False,
+                                                     evaluate_on=evaluate_on)}
     # first, eval and save what is the performance before training
 
-    result['pre_eval'] = train_and_eval_single_step(args, train_dataset, *aligneds, *features, *gold_files,
-                                                    run_nr='eval', keep_predictions=bool(keep_predictions), train=False,
-                                                    evaluate_on=evaluate_on)
     click.echo(f"Results: {json.dumps(result['pre_eval'], indent=4)}")
     # run hyperparam optimisation
-    with tempfile.TemporaryDirectory() as tempdir:
-        predictions_folder = keep_predictions
-        for i in trange(num_hpopt_runs):
-            parameters, trial_index = ax_client.get_next_trial()
-            logger.info(f"Trying parameters: {parameters}")
-            single_step_args = deepcopy(kwargs)
-            single_step_args.update(parameters)
-            args = Args(**single_step_args)
-            args.predictions_folder = str(predictions_folder)
-            trial_result = train_and_eval_single_step(args, train_dataset, *aligneds, *features, *gold_files, run_nr=i,
-                                                      keep_predictions=bool(keep_predictions), num_runs=runs_per_trial,
-                                                      evaluate_on=evaluate_on)
+    predictions_folder = keep_predictions
+    for i in trange(hyperparam_opt_runs):
+        parameters, trial_index = ax_client.get_next_trial()
+        logger.info(f"Trying parameters: {parameters}")
+        single_step_args = deepcopy(kwargs)
+        single_step_args.update(parameters)
+        args = Args(**single_step_args)
+        args.predictions_folder = str(predictions_folder)
+        trial_result = train_and_eval_single_step(args, train_dataset, *aligneds, *features, original_dev_dataset,
+                                                  *gold_files, run_nr=i, num_runs=runs_per_trial,
+                                                  evaluate_on=evaluate_on)
+        #
+        if optimize_consistency:
+            assert evaluate_on == 'eoi'
+            mean = trial_result['consistency']
+        else:
             mean = trial_result['overall' if evaluate_on == 'eoi' else 'EMRelaxed']
-            if runs_per_trial > 1:
-                mean, var, ci = mean
-            click.echo(f"Result: {mean}")
-            click.echo(f"Results: {json.dumps(trial_result, indent=4)}")
-            result["trials"].append(trial_result)
-            result['tried_params'][i].append(parameters)
-            ax_client.complete_trial(trial_index=trial_index, raw_data=mean)
+        if runs_per_trial > 1:
+            mean, var, ci = mean
+        if original_dev_dataset:
+            logger.info(f"Mean: ({mean} * 100 + {trial_result['original']})/2")
+            mean = (mean * 100 + trial_result['original']) / 2
+
+        trial_result["mean"] = mean
+
+        logger.info(f"Result: {mean}")
+        logger.info(f"Results: {json.dumps(trial_result, indent=4)}")
+        result["trials"].append(trial_result)
+        result['tried_params'][i].append(parameters)
+        ax_client.complete_trial(trial_index=trial_index, raw_data=mean)
     best_params, metrics = ax_client.get_best_parameters()
     result['best_params'] = best_params
     result['best_metrics'] = metrics
