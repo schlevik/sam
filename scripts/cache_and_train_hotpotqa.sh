69b5540a4bb40e9ce5320c361b1abd81f983692f
schlevik <viktor@schlegel-online.de>
2020-08-12 13:22:26 +0100

    fix cache location

--- scripts/cache_and_train_hotpotqa.sh
+++ scripts/cache_and_train_hotpotqa.sh
@@ -5,11 +5,11 @@ python main.py cache-examples data/datasets/hotpotqa/train.json \
   --out-folder "$CACHE_LOCATION" --dataset-only
 echo "Running distributed training...."
 python -m torch.distributed.launch --nproc_per_node=4 --use_env main.py \
-  --debug train "$CACHE_LOCATION" \
+  --debug train "$CACHE_LOCATION/train-{$MODEL}.bin" \
   --model-path "$MODEL" --model-type "$MODEL_TYPE" --eval-file data/datasets/hotpotqa/dev.json \
   --save-model-folder "$SAVE_TO" --do-eval-after-training \
   --per-gpu-train-batch-size "$BATCH_SIZE" --max-answer-length 30 --gradient-accumulation-steps "$ACC_STEPS" \
   --fp16 --debug-features --save-steps 0 --num-train-epochs 2
-echo "Removing $CACHE_LOCATION"
-rm -fr "$CACHE_LOCATION"
+#echo "Removing $CACHE_LOCATION"
+#rm -fr "$CACHE_LOCATION"
 echo "Done!"
