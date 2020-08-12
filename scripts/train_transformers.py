7e565a046b91facdd1be772841b3f847f4f3499c
schlevik <viktor@schlegel-online.de>
2020-08-12 15:45:15 +0100

    fix caching and use parallel dataloader

--- scripts/train_transformers.py
+++ scripts/train_transformers.py
@@ -198,7 +198,7 @@ def do_train(args: Args, train_dataset, model, tokenizer):
 
     args.train_batch_size = args.per_gpu_train_batch_size * max(1, args.n_gpu)
     train_sampler = RandomSampler(train_dataset) if args.local_rank == -1 else DistributedSampler(train_dataset)
-    train_dataloader = DataLoader(train_dataset, sampler=train_sampler, batch_size=args.train_batch_size)
+    train_dataloader = DataLoader(train_dataset, num_workers=4, sampler=train_sampler, batch_size=args.train_batch_size)
 
     if args.max_steps > 0:
         t_total = args.max_steps
