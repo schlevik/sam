2af2a40a90c659741670d51b224498a0caeaf1f3
schlevik <viktor@schlegel-online.de>
2020-08-14 12:14:23 +0100

    eval on wikihop instead of hotpot

--- scripts/cache_and_train_wikihop.sh
+++ scripts/cache_and_train_wikihop.sh
@@ -6,10 +6,10 @@ python main.py cache-examples data/datasets/wikihop/train.json \
 echo "Running distributed training...."
 python -m torch.distributed.launch --nproc_per_node=4 --use_env main.py \
   --debug train "$CACHE_LOCATION/train-$MODEL.bin" \
-  --model-path "$MODEL" --model-type "$MODEL_TYPE" --eval-file data/datasets/hotpotqa/dev.json \
+  --model-path "$MODEL" --model-type "$MODEL_TYPE" --eval-file data/datasets/wikihop/dev.json \
   --save-model-folder "$SAVE_TO" --do-eval-after-training \
   --per-gpu-train-batch-size "$BATCH_SIZE" --max-answer-length 30 --gradient-accumulation-steps "$ACC_STEPS" \
-  --fp16 --debug-features --save-steps 0 --num-train-epochs 2 --num-workers 8
+  --fp16 --debug-features --save-steps 0 --num-train-epochs 3 --num-workers 8
 #echo "Removing $CACHE_LOCATION"
 #rm -fr "$CACHE_LOCATION"
 echo "Done!"
